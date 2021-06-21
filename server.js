69fbc285809cfbebd04d66313dbffb953f00433c
Ayyoub Al keyyam <ayoub.alqeam@gmail.com>
2021-06-21 20:46:54 +0300

    سثقرثقزتس ثيهفثي

--- server.js
+++ server.js
@@ -3,22 +3,22 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const getBooks = require("./controller/book.controller");
-// const axios = require('axios');
-const seedUserData = require('./models/user.model')
 require("dotenv").config();
+// const axios = require('axios');
+// const seedUserData = require('./models/user.model')
 app.use(cors());
-const port = process.env.PORT;
+const PORT = process.env.PORT;
 mongoose.connect("mongodb://localhost:27017/myFavoritebooks", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-seedUserData();
+// seedUserData();
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
 app.get("/books", getBooks);
 
-server.listen(port, () => {
-  console.log(`Listening on ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server started on ${PORT}`);
 });
