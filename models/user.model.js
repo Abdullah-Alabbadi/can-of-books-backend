e9c681c5e37c7975f9ecf7cdda941d1815cf507f
Ayyoub Al keyyam <ayoub.alqeam@gmail.com>
2021-06-21 19:50:40 +0300

    book scheme edited

--- models/user.model.js
+++ models/user.model.js
@@ -23,9 +23,16 @@ const seedUserData = () => {
     const newUser = new userModel({
         email: 'abdaullah20000@gmail.com',
         books: [
-            { name: 'book1' },
-            { name: 'book2' },
-            { name: 'book3' }
+            {
+                name: 'Living in the Light: A guide to personal transformation',
+                description: 'Living in the Light: A Guide to Personal and Planetary Transformation is just that, but so much more. This is self-help for those who have real There comes a time for many when their self-help journey inevitably takes on a spiritual connotation given the extent to how deep their attempts to become the best version of themself go.',
+                url: 'https://ecdn.teacherspayteachers.com/thumbitem/Living-in-the-Light-A-guide-to-personal-transformation-4998424-1572904693/original-4998424-1.jpg'
+            },
+            {
+                name: 'The Choice: Embrace the Possible',
+                description: 'Its 1944 and sixteen-year-old ballerina and gymnast Edith Eger is sent to Auschwitz. Separated from her parents on arrival, she endures unimaginable experiences, including being made to dance for the infamous Josef Mengele. When the camp is finally liberated, she is pulled from a pile of bodies, barely alive.',
+                url: 'https://kbimages1-a.akamaihd.net/2b4b2761-dee6-4592-8c33-934d1c3b9046/353/569/90/False/the-choice-80.jpg'
+            }
         ]
     });
 
--- server.js
+++ server.js
@@ -1,23 +1,24 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose');
-const cors = require('cors');
-const getBooks = require('./controller/book.controller');
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const getBooks = require("./controller/book.controller");
 // const axios = require('axios');
-// const seedUserData = require('./models/user.model')
-require('dotenv').config();
-app.use(cors())
-const port= process.env.PORT;
-mongoose.connect('mongodb://localhost:27017/myFavoritebooks',
-    { useNewUrlParser: true, useUnifiedTopology: true }
-);
+const seedUserData = require('./models/user.model')
+require("dotenv").config();
+app.use(cors());
+const port = process.env.PORT;
+mongoose.connect("mongodb://localhost:27017/myFavoritebooks", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-// seedUserData();
-app.get('/',
-    function (req, res) {
-        res.send('Hello World')
-    })
-app.get('/books', getBooks)
-app.listen(port, () => {
-    // console.log(`Server started on ${PORT}`);
+seedUserData();
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+app.get("/books", getBooks);
+
+server.listen(port, () => {
+  console.log(`Listening on ${port}`);
 });
