8550c0ec636fbfe132b123791f8d3feb7751e9df
Ayyoub Al keyyam <ayoub.alqeam@gmail.com>
2021-06-22 19:55:53 +0300

    backend lab-13 done

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
-const getBooks = require("./controller/book.controller");
+app.use(cors());
+app.use(express.json());
+const {
+  getBooks,
+  createBook,
+  deleteBook
+} = require("./controller/book.controller");
 require("dotenv").config();
 // const axios = require('axios');
-// const seedUserData = require('./models/user.model')
-app.use(cors());
+// const {seedUserData} = require('./models/user.model')
 const PORT = process.env.PORT;
 mongoose.connect("mongodb://localhost:27017/myFavoritebooks", {
   useNewUrlParser: true,
@@ -18,7 +23,8 @@ app.get("/", function (req, res) {
   res.send("Hello World");
 });
 app.get("/books", getBooks);
-
+app.post('/book', createBook);
+app.delete('/book/:book_idx', deleteBook)
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
 });
